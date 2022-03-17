f07ba3099119f15c9c49177f82ca5bf3e82801fe
Queiroz Sousa <queirozmacamo82@gmail.com>
2022-03-17 19:19:27 +0200

    Componet Button

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import styles from './Home.module.css';
 
 import Savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton';
 
 function Home(){
     return (
         <section className={styles.home_container}>
             <h1>Bem-Vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <a href="/">Criar Projecto</a>
+            <LinkButton to='/newproject' text='Criar Projeto'/>
             <img src={Savings} alt="Costs" />
         </section>
     )
