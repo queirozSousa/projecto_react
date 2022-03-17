b983985ab17a40c19ec5e800ae9b145e4c404475
Queiroz Sousa <queirozmacamo82@gmail.com>
2022-03-17 19:09:23 +0200

    Home Layout

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,16 @@
+import styles from './Home.module.css';
+
+import Savings from '../../img/savings.svg'
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href="/">Criar Projecto</a>
+            <img src={Savings} alt="Costs" />
+        </section>
+    )
 }
 
 export default Home
