809137403f66900891e2abca5adb80520a964771
Queiroz Sousa <queirozmacamo82@gmail.com>
2022-03-20 18:27:00 +0200

    Form Component...

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,14 @@
+import styles from './NewProject.module.css'
+import ProjecForm from '../project/ProjectForm'
+
+
 function NewProject(){
     return(
-        <h1>Pagina de Novo Projecto</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projecto</h1>
+            <p>Crie o seu Projecto para depois adicionar os servicos</p>
+            <ProjecForm />
+        </div>
     )
 }
 
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,23 @@
+function ProjecForm(){
+    return(
+        <form>
+            <div>
+                <input type="text" placeholder="Insira o nome do projecto" />
+            </div>
+            <div>
+                <input type="number" placeholder="Insira o orcamento total"/>
+            </div>
+            <div>
+                <select name="category_id">
+                 <option disabled>Selecione a categoria</option>
+                </select>
+            </div>
+            <div>
+                <input type="submit" value='Criar Projecto' />
+            </div>
+            
+        </form>
+    )
+}
+
+export default ProjecForm
