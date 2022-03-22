1147e2bf1ffaaf9947c7832ba11a28fb333902b9
Queiroz Sousa <queirozmacamo82@gmail.com>
2022-03-22 20:28:10 +0200

    Component Form

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,18 +1,13 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
 import styles from './Project.module.css'
 
 function ProjecForm(){
     return(
         <form className={styles.form}>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome'/>
-            <div>
-                <input type="number" placeholder="Insira o orcamento total"/>
-            </div>
-            <div>
-                <select name="category_id">
-                 <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
+            <Input type='number' text='Orcamento do projeto' name='budget' placeholder='Insira o nome do projeto'/>
+            <Select name='category_id' text='Selecione a categoria' />
             <div>
                 <input type="submit" value='Criar Projecto' />
             </div>
